9f16873e528e569868454ca2b716ae2b1245f189
Isabelly Cristina <isabelly.c.sales@aluno.senai.br>
2026-08-04 19:42:10 -0300

    arquivos deletados

--- src/app/formulario/formulario.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-formulario',
-  imports: [],
-  templateUrl: './formulario.html',
-  styleUrl: './formulario.css',
-})
-export class Formulario {}
