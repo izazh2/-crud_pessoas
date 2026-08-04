ecc79d71f033ab56e1b8212a8e188b9344371d45
Isabelly Cristina <isabelly.c.sales@aluno.senai.br>
2026-08-04 18:08:44 -0300

    diversas alterações

--- src/app/crud-pessoa/pessoa.ts
+++
@@ -1,9 +0,0 @@
-export class Pessoa {
-    id: number = 0
-    nome : string = ''
-    email : string = ''
-    cpf : string = ''
-    data_nascimento : string = ''
-    uf : string = ''
-    municipio : string = ''
-}
