d50596cff3cd7d099e3da93a9347eb150387c167
Isabelly Cristina <isabelly.c.sales@aluno.senai.br>
2026-08-04 21:16:19 -0300

    diversas alterações no arquivo src/app/app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Formulario } from './component/formulario/formulario';
+import { HomeComponent } from './component/home-component/home-component';
+
+
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch : 'full'
+    },
+    
+    {
+        path: 'home',
+        component : HomeComponent
+    },
+
+    {
+        path: 'cadastro',
+        component: Formulario
+    }
+];
