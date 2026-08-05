b48d1f312e9c7a7250ec5c5651014940ebda8f96
Isabelly Cristina <isabelly.c.sales@aluno.senai.br>
2026-08-04 21:41:53 -0300

    diversas alterações no arquivo

--- src/app/component/formulario/formulario.ts
+++ src/app/component/formulario/formulario.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import { PessoaService } from '../../services/pessoa-service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
-  imports: [],
+  imports: [PessoaService, FormsModule],
   templateUrl: './formulario.html',
   styleUrl: './formulario.css',
 })
-export class Formulario {}
+export class Formulario {
+  nome = ''
+  email = ''
+  cpf = ''
+  dataNascimento = ''
+
+  constructor(private pessoaService : PessoaService){}
+
+  save(){
+    this.pessoaService.adicionar({
+      id: 1,
+      nome: this.nome,
+      email: this.email,
+      dataNascimento : this.dataNascimento
+    })
+  }
+
+}
